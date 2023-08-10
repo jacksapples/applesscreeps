df4dfe6ff008da12b94a908978b7d34666669902
jacksapples <jjkc1988@gmail.com>
2023-08-10 16:46:47 -0500

    new harvester

--- Test1/role.harvester.js
+++ Test1/role.harvester.js
@@ -10,40 +10,74 @@ module.exports = {
             creep.memory.working = true;
         }
 
-        // If working, transfer energy
+        // If working, transfer energy based on priority
         if (creep.memory.working) {
-            // Similar logic to the original for transferring energy...
-        } else {
-            // Use stored sourceId if available
-            if (!creep.memory.sourceId) {
-                // Find the closest active source that's not in a hostile zone
-                const pathOpts = {
-                    costCallback: function(roomName, costMatrix) {
-                        if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
-                            costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
-                        }
-                    }
-                };
-                const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
-                    filter: (s) => {
-                        return !Memory.hostileZone || (s.pos.x !== Memory.hostileZone.x && s.pos.y !== Memory.hostileZone.y);
-                    },
-                    pathOpts
+            let target = null;
+
+            // Priority: Spawns and extensions
+            if (!target) {
+                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                    filter: s => (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) &&
+                                 s.energy < s.energyCapacity
                 });
-                
-                if (source) {
-                    creep.memory.sourceId = source.id;
-                }
             }
 
-            // Now, make the harvester go to its assigned source
-            const targetSource = Game.getObjectById(creep.memory.sourceId);
-            if (targetSource && creep.harvest(targetSource) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetSource);
+            // Priority: Towers
+            if (!target) {
+                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                    filter: s => s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity
+                });
+            }
+
+            // Priority: Storage
+            if (!target) {
+                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                    filter: s => s.structureType == STRUCTURE_STORAGE && _.sum(s.store) < s.storeCapacity
+                });
+            }
+
+            // Priority: Containers
+            if (!target) {
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => s.structureType == STRUCTURE_CONTAINER && _.sum(s.store) < s.storeCapacity
+                });
+            }
+
+            // Transfer energy or move closer to the target
+            if (target) {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+                // Fallback: Upgrade the controller if there's absolutely nowhere to put energy
+                if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                }
+            }
+        } else {
+            // Harvesting logic extracted from the previous content
+            const pathOpts = {
+                costCallback: function(roomName, costMatrix) {
+                    if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
+                        costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
+                    }
+                }
+            };
+            const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+                filter: (s) => {
+                    return !Memory.hostileZone || (s.pos.x !== Memory.hostileZone.x && s.pos.y !== Memory.hostileZone.y);
+                },
+                pathOpts
+            });
+
+            if (source) {
+                if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
             }
         }
 
-        // If the creep is under attack, move to a safe location and mark the current position as hostile
+        // If the creep is under attack, move to a safe location
         if (creep.hits < creep.hitsMax) {
             const hostilePos = creep.pos;
             Memory.hostileZone = {
