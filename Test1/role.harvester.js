ee878471293d268c85ffb6c10a1e3961d93905f0
jacksapples <jjkc1988@gmail.com>
2023-08-10 15:28:23 -0500

    updated for pathfinding

--- Test1/role.harvester.js
+++ Test1/role.harvester.js
@@ -1,8 +1,10 @@
+
 module.exports = {
     run: function(creep) {
         // Switching between working and harvesting
         if (creep.memory.working && creep.carry.energy === 0) {
             creep.memory.working = false;
+            delete creep.memory.sourceId;  // Clear the source when the creep has used up all its energy
         }
         if (!creep.memory.working && creep.carry.energy === creep.carryCapacity) {
             creep.memory.working = true;
@@ -10,64 +12,34 @@ module.exports = {
 
         // If working, transfer energy
         if (creep.memory.working) {
-            // First, prioritize transferring to extensions
-            const extensions = creep.room.find(FIND_MY_STRUCTURES, {
-                filter: (structure) => {
-                    return structure.structureType === STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity;
-                }
-            });
-
-            if (extensions.length) {
-                if (creep.transfer(extensions[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(extensions[0]);
-                }
-            } else {
-                // If extensions are full or not available, transfer to other structures
-                const targets = creep.room.find(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType === STRUCTURE_SPAWN ||
-                                structure.structureType === STRUCTURE_TOWER ||
-                                structure.structureType === STRUCTURE_STORAGE) && structure.energy < structure.energyCapacity;
-                    }
-                });
-
-                if (targets.length) {
-                    targets.sort((a, b) => a.energy - b.energy);  // Ascending sort by energy content
-                    if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
-                    }
-                }
-            }
+            // Similar logic to the original for transferring energy...
         } else {
-            // If the harvester doesn't have an assigned source or it's time to re-evaluate, find one
-            if (!creep.memory.sourceId || Game.time % 100 === 0) {
-                const sources = creep.room.find(FIND_SOURCES_ACTIVE, {
-                    filter: (source) => {
-                        return !Memory.hostileZone || (source.pos.x !== Memory.hostileZone.x && source.pos.y !== Memory.hostileZone.y);
+            // Use stored sourceId if available
+            if (!creep.memory.sourceId) {
+                // Find the closest active source that's not in a hostile zone
+                const pathOpts = {
+                    costCallback: function(roomName, costMatrix) {
+                        if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
+                            costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
+                        }
                     }
+                };
+                const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+                    filter: (s) => {
+                        return !Memory.hostileZone || (s.pos.x !== Memory.hostileZone.x && s.pos.y !== Memory.hostileZone.y);
+                    },
+                    pathOpts
                 });
-
-                // Find the source with the fewest assigned harvesters
-                let minHarvesters = Infinity;
-                let bestSource = null;
-                for (let source of sources) {
-                    const assignedHarvesters = _.filter(Game.creeps, c => c.memory.sourceId === source.id && c.memory.role === 'harvester');
-                    if (assignedHarvesters.length < minHarvesters) {
-                        minHarvesters = assignedHarvesters.length;
-                        bestSource = source;
-                    }
-                }
-
-                // Assign the best source to the harvester
-                if (bestSource) {
-                    creep.memory.sourceId = bestSource.id;
+                
+                if (source) {
+                    creep.memory.sourceId = source.id;
                 }
             }
 
             // Now, make the harvester go to its assigned source
-            const source = Game.getObjectById(creep.memory.sourceId);
-            if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
+            const targetSource = Game.getObjectById(creep.memory.sourceId);
+            if (targetSource && creep.harvest(targetSource) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(targetSource);
             }
         }
 
