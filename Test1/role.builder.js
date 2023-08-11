9651ff1d4940450d6562574050b53026d80f05e6
jacksapples <jjkc1988@gmail.com>
2023-08-11 12:47:11 -0500

    moving old builder back

--- Test1/role.builder.js
+++ Test1/role.builder.js
@@ -1,29 +1,121 @@
 
-var roleBuilder = {
-    run: function (creep) {
-        if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
-            creep.memory.building = false;
-            creep.say('🔄 harvest');
+module.exports = {
+    
+run: function(creep) {
+    let pathOpts = {
+        costCallback: function(roomName, costMatrix) {
+            if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
+                costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
+            }
         }
-        if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
-            creep.memory.building = true;
-            creep.say('🚧 build');
+    };
+
+        // Switching between working and harvesting
+        if (creep.memory.working && creep.carry.energy === 0) {
+            creep.memory.working = false;
+        }
+        if (!creep.memory.working && creep.carry.energy === creep.carryCapacity) {
+            creep.memory.working = true;
         }
 
-        if (creep.memory.building) {
-            let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if (targets.length) {
-                if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+        // Priority hierarchy for building
+        
+const PRIORITY_STRUCTURES = [
+    STRUCTURE_CONTAINER,
+    STRUCTURE_STORAGE,
+    STRUCTURE_TOWER,
+    STRUCTURE_ROAD,
+    STRUCTURE_EXTENSION,
+    STRUCTURE_SPAWN,
+    STRUCTURE_WALL,
+    STRUCTURE_RAMPART,
+    // ... add more structures as required
+];
+
+        // If working, find the most important construction site
+        if (creep.memory.working) {
+            let target = null;
+            for (let type of PRIORITY_STRUCTURES) {
+                if (!target) {
+                    target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, {
+                        filter: (site) => site.structureType === type
+                    });
+                }
+            }
+
+            // Build or move to the construction site
+            if (target) {
+                if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+                // If no construction sites, repair the most damaged structures
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: s => s.hits < s.hitsMax && s.structureType !== STRUCTURE_WALL,
+                    sorting: (a, b) => a.hits - b.hits  // Prioritize the most damaged
+                });
+                if (target) {
+                    if (creep.repair(target) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                } else {
+                    // If nothing to repair, then upgrade the controller
+                    if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller);
+                    }
                 }
             }
         } else {
-            let sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+            
+    
+    // Find the closest energy storage (either source, storage, or container)
+    pathOpts = {
+        costCallback: function(roomName, costMatrix) {
+            if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
+                costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
             }
         }
-    }
-};
+    };
 
-module.exports = roleBuilder;
+    const energySources = creep.pos.findClosestByPath([FIND_SOURCES_ACTIVE, FIND_STRUCTURES], {
+        filter: (s) => {
+            return (!Memory.hostileZone || (s.pos.x !== Memory.hostileZone.x && s.pos.y !== Memory.hostileZone.y)) && 
+                   (s.structureType === undefined || s.structureType === STRUCTURE_STORAGE || s.structureType === STRUCTURE_CONTAINER);
+        },
+        pathOpts
+    });
+
+    if (energySources) {
+        if (creep.harvest(energySources) === ERR_NOT_IN_RANGE && creep.withdraw(energySources, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(energySources);
+        }
+    }
+
+    pathOpts = {
+        costCallback: function(roomName, costMatrix) {
+            if (Memory.hostileZone && Memory.hostileZone.roomName === roomName && Game.time < Memory.hostileZone.endTick) {
+                costMatrix.set(Memory.hostileZone.x, Memory.hostileZone.y, 255);
+            }
+        }
+    };
+    const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+        filter: (s) => {
+            return !Memory.hostileZone || (s.pos.x !== Memory.hostileZone.x && s.pos.y !== Memory.hostileZone.y);
+        },
+        pathOpts
+    });
+
+    if (source) {
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+        }
+    }
+    
+        }
+
+        // If the creep is under attack, move to a safe location
+        if (creep.hits < creep.hitsMax) {
+            creep.moveTo(creep.room.controller);  // Move to a safer location
+        }
+    }
+}
