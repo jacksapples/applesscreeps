f96c2c457bee56264ad4c60b6d13476c97979db5
jacksapples <jjkc1988@gmail.com>
2023-08-14 20:04:17 -0500

    error handiling update

--- role.builder.js
+++ role.builder.js
@@ -3,27 +3,31 @@ var utils = require('utils');
 
 module.exports = {
     run: function(creep) {
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.building = false;
-        }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-            creep.memory.building = true;
-        }
+        try {
+            if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+                creep.memory.building = false;
+            }
+            if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+                creep.memory.building = true;
+            }
 
-        if(creep.memory.building) {
-            var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-            if(target) {
-                if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
+            if(creep.memory.building) {
+                var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+                if(target) {
+                    if(creep.build(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                        this.buildRoad(creep);
+                    }
+                }
+            } else {
+                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
                     this.buildRoad(creep);
                 }
             }
-        } else {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-                this.buildRoad(creep);
-            }
+        } catch (error) {
+            console.log('Error in builder role for creep ' + creep.name + ':', error.stack);
         }
     },
 
--- role.upgrader.js
+++ role.upgrader.js
@@ -3,24 +3,28 @@ var utils = require('utils');
 
 module.exports = {
     run: function(creep) {
-        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
-        }
-        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
-        }
+        try {
+            if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+                creep.memory.upgrading = false;
+            }
+            if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+                creep.memory.upgrading = true;
+            }
 
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-                this.buildRoad(creep);
+            if(creep.memory.upgrading) {
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                    this.buildRoad(creep);
+                }
+            } else {
+                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                    this.buildRoad(creep);
+                }
             }
-        } else {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
-                this.buildRoad(creep);
-            }
+        } catch (error) {
+            console.log('Error in upgrader role for creep ' + creep.name + ':', error.stack);
         }
     },
 
