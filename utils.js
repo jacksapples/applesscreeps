c3ff7d0fb41f287d9955b3af2766f36734d04a00
jacksapples <jjkc1988@gmail.com>
2023-08-14 20:18:06 -0500

    ---

--- utils.js
+++ utils.js
@@ -1,9 +1,14 @@
 
-var utils = {
+const MIN_HARVESTERS = 3;
+const MIN_UPGRADERS = 2;
+const MIN_BUILDERS = 2;
+const MIN_REPAIRERS = 1;
+const MIN_DEFENDERS = 1;
 
+module.exports = {
     cleanupMemory: function() {
-        for (let name in Memory.creeps) {
-            if (Game.creeps[name] === undefined) {
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
                 delete Memory.creeps[name];
             }
         }
@@ -11,44 +16,27 @@ var utils = {
 
     manageCreeps: function(spawn) {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         var defenders = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender');
         var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
 
-        if (harvesters.length < 2) {
+        if(harvesters.length < MIN_HARVESTERS) {
             this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'harvester');
-        } else if (upgraders.length < 1) {
+        } else if(upgraders.length < MIN_UPGRADERS) {
             this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'upgrader');
-        } else if (builders.length < 1) {
+        } else if(builders.length < MIN_BUILDERS) {
             this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'builder');
-        } else if (defenders.length < 1) {
-            this.spawnCreep(spawn, [ATTACK, ATTACK, MOVE, MOVE], 'defender');
-        } else if (repairers.length < 1) {
+        } else if(defenders.length < MIN_DEFENDERS) {
+            this.spawnCreep(spawn, [TOUGH, ATTACK, MOVE], 'defender');
+        } else if(repairers.length < MIN_REPAIRERS) {
             this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'repairer');
         }
     },
 
     spawnCreep: function(spawn, body, role) {
-        var newName = role + Game.time;
-        var memory = { role: role, working: false };
-
-        // Explicit memory initialization based on role
-        switch (role) {
-            case 'harvester':
-            case 'upgrader':
-                memory.upgrading = false;
-                memory.harvesting = true;
-                break;
-            case 'builder':
-                memory.building = false;
-                break;
-            default:
-                break;
-        }
-
-        spawn.spawnCreep(body, newName, { memory: memory });
+        var newName = role.charAt(0).toUpperCase() + role.slice(1) + Game.time;
+        spawn.spawnCreep(body, newName, {memory: {role: role}});
     }
 };
 
-module.exports = utils;
