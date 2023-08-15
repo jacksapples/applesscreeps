d894a9464af5ccc917c7d672d502fb5f40d2df9a
jacksapples <jjkc1988@gmail.com>
2023-08-14 20:08:10 -0500

    new utils

--- utils.js
+++ utils.js
@@ -1,14 +1,9 @@
 
-const MIN_HARVESTERS = 3;
-const MIN_UPGRADERS = 2;
-const MIN_BUILDERS = 2;
-const MIN_REPAIRERS = 1;
-const MIN_DEFENDERS = 1;
+var utils = {
 
-module.exports = {
     cleanupMemory: function() {
-        for(var name in Memory.creeps) {
-            if(!Game.creeps[name]) {
+        for (let name in Memory.creeps) {
+            if (Game.creeps[name] === undefined) {
                 delete Memory.creeps[name];
             }
         }
@@ -16,56 +11,44 @@ module.exports = {
 
     manageCreeps: function(spawn) {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         var defenders = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender');
         var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
 
-        if(harvesters.length < MIN_HARVESTERS) {
-            this.spawnCreep(spawn, this.getBodyParts('harvester'), 'harvester');
-        } else if(upgraders.length < MIN_UPGRADERS) {
-            this.spawnCreep(spawn, this.getBodyParts('upgrader'), 'upgrader');
-        } else if(builders.length < MIN_BUILDERS) {
-            this.spawnCreep(spawn, this.getBodyParts('builder'), 'builder');
-        } else if(defenders.length < MIN_DEFENDERS) {
-            this.spawnCreep(spawn, this.getBodyParts('defender'), 'defender');
-        } else if(repairers.length < MIN_REPAIRERS) {
-            this.spawnCreep(spawn, this.getBodyParts('repairer'), 'repairer');
+        if (harvesters.length < 2) {
+            this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'harvester');
+        } else if (upgraders.length < 1) {
+            this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'upgrader');
+        } else if (builders.length < 1) {
+            this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'builder');
+        } else if (defenders.length < 1) {
+            this.spawnCreep(spawn, [ATTACK, ATTACK, MOVE, MOVE], 'defender');
+        } else if (repairers.length < 1) {
+            this.spawnCreep(spawn, [WORK, CARRY, MOVE], 'repairer');
         }
     },
 
     spawnCreep: function(spawn, body, role) {
-        var newName = role.charAt(0).toUpperCase() + role.slice(1) + Game.time;
-        spawn.spawnCreep(body, newName, {memory: {role: role}});
-    },
+        var newName = role + Game.time;
+        var memory = { role: role, working: false };
 
-    getBodyParts: function(role) {
-        var body = [];
-        var availableEnergy = Game.spawns['Spawn1'].room.energyCapacityAvailable;
-
-        if (role === 'harvester' || role === 'upgrader') {
-            var numOfParts = Math.floor(availableEnergy / 200);
-            for (let i = 0; i < numOfParts; i++) {
-                body.push(WORK);
-                body.push(CARRY);
-                body.push(MOVE);
-            }
-        } else if (role === 'builder' || role === 'repairer') {
-            var numOfParts = Math.floor(availableEnergy / 200);
-            for (let i = 0; i < numOfParts; i++) {
-                body.push(WORK);
-                body.push(CARRY);
-                body.push(MOVE);
-            }
-        } else if (role === 'defender') {
-            var numOfParts = Math.floor(availableEnergy / 150);
-            for (let i = 0; i < numOfParts; i++) {
-                body.push(TOUGH);
-                body.push(ATTACK);
-                body.push(MOVE);
-            }
+        // Explicit memory initialization based on role
+        switch (role) {
+            case 'harvester':
+            case 'upgrader':
+                memory.upgrading = false;
+                memory.harvesting = true;
+                break;
+            case 'builder':
+                memory.building = false;
+                break;
+            default:
+                break;
         }
-        return body;
+
+        spawn.spawnCreep(body, newName, { memory: memory });
     }
 };
 
+module.exports = utils;
