78170b55597e569a5ccdf410b8e55e847e212b03
jacksapples <jjkc1988@gmail.com>
2023-08-14 20:16:14 -0500

    --

--- role.builder.js
+++ role.builder.js
@@ -1,48 +1,25 @@
 
-var utils = require('utils');
-
 module.exports = {
     run: function(creep) {
-        try {
-            if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-                creep.memory.building = false;
-            }
-            if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-                creep.memory.building = true;
-            }
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+        }
+        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+        }
 
-            if(creep.memory.building) {
-                var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-                if(target) {
-                    if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target);
-                        this.buildRoad(creep);
-                    }
-                }
-            } else {
-                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source);
-                    this.buildRoad(creep);
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
                 }
             }
-        } catch (error) {
-            console.log('Error in builder role for creep ' + creep.name + ':', error.stack);
-        }
-    },
-
-    buildRoad: function(creep) {
-        var atPos = creep.pos.lookFor(LOOK_STRUCTURES);
-        var roadFound = false;
-        for (let i in atPos) {
-            if (atPos[i].structureType == STRUCTURE_ROAD) {
-                roadFound = true;
-                break;
+        } else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
-        }
-        if (!roadFound) {
-            creep.pos.createConstructionSite(STRUCTURE_ROAD);
         }
     }
-    
-}
+};
